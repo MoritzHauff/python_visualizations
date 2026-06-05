dced174f52b256db051731077b4273e2b154fbe1
MoritzH <29129441+MoritzHauff@users.noreply.github.com>
2026-06-05 18:15:58 +0200

    ADD show details of selection

--- src/map_bubble/explore_nf_membership_clusters.py
+++ src/map_bubble/explore_nf_membership_clusters.py
@@ -4,8 +4,8 @@ __generated_with = "0.21.1"
 app = marimo.App(width="medium")
 
 with app.setup:
-    import io
     import json
+    import locale
     import requests
     import marimo as mo
     import pandas as pd
@@ -16,6 +16,9 @@ with app.setup:
     from cryptography.fernet import InvalidToken
 
     from crypto.encryption import get_key, Fernet
+
+    # Set the locale to German
+    locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
 
 
 @app.cell(hide_code=True)
@@ -51,19 +54,17 @@ def _(df, ui_settings, ui_settings_show_raw_df):
     mo.accordion(
         {
             "Einstellungen": ui_settings,
-            "Rohdaten": df if ui_settings_show_raw_df.value else mo.md("Aktiviere \"Rohdaten\", in den Einstellungen."),
-            "Filter": mo.md("Hier kommen bald Filter dazu.")
+            "Rohdaten": df if ui_settings_show_raw_df.value else mo.md('Aktiviere "Rohdaten", in den Einstellungen.'),
+            "Filter": mo.md("Hier kommen bald Filter dazu."),
         },
-        multiple=True
+        multiple=True,
     )
     return
 
 
 @app.function
 def get_data_air_traffic() -> pd.DataFrame:
-    df = pd.read_csv(
-        "https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv"
-    )
+    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv")
     return df
 
 
@@ -83,10 +84,14 @@ def download_file(url: str) -> bytes:
 @app.function
 def download_data(password) -> dict:
     """Downloads the encrypted data file and the corresponding salt, derives the encryption key using the provided password, and decrypts the data."""
-    salt = download_file("https://raw.githubusercontent.com/MoritzHauff/python_visualizations/refs/heads/main/src/map_bubble/nf_membership/nf_membership_data.salt")
+    salt = download_file(
+        "https://raw.githubusercontent.com/MoritzHauff/python_visualizations/refs/heads/main/src/map_bubble/nf_membership/nf_membership_data.salt"
+    )
     # print(f"Accessed salt successfully: {salt} type: {type(salt)}")
 
-    file = download_file("https://raw.githubusercontent.com/MoritzHauff/python_visualizations/refs/heads/main/src/map_bubble/nf_membership/nf_membership_data.enc")
+    file = download_file(
+        "https://raw.githubusercontent.com/MoritzHauff/python_visualizations/refs/heads/main/src/map_bubble/nf_membership/nf_membership_data.enc"
+    )
 
     # Only after both downloads were sucessful, we can proceed to get the key and decrypt the data
     key = get_key(password, salt)
@@ -97,7 +102,7 @@ def download_data(password) -> dict:
     except InvalidToken:
         raise Exception("Decryption failed. Please check your password and try again.")
     data_json = json.loads(data_bytes.decode())
-    
+
     return data_json
 
 
@@ -124,20 +129,24 @@ def _(COL_GROUP, ui_text_password):
             return False, pd.DataFrame()
 
         if len(df_locations) != len(df_memberships):
-            print("**Warnung:** Die Anzahl der Ortsgruppen in den beiden Datensätzen stimmt nicht überein. Es könnte sein, dass einige Gruppen in einem Datensatz fehlen.")
-        
+            print(
+                "**Warnung:** Die Anzahl der Ortsgruppen in den beiden Datensätzen stimmt nicht überein. Es könnte sein, dass einige Gruppen in einem Datensatz fehlen."
+            )
+
         # clean data
         df_locations = df_locations.rename(columns={"Unnamed: 6": "Koordinaten Kommentar"})
         df_locations = df_locations.rename(columns={"Gruppenbezeichnung": COL_GROUP})
-        #df_locations[COL_GROUP] = df_locations[COL_GROUP].str.replace("Bezirk München", "Bz München")   
+        # df_locations[COL_GROUP] = df_locations[COL_GROUP].str.replace("Bezirk München", "Bz München")
 
         df_memberships = df_memberships.rename(columns={"Gruppenbezeichnung": COL_GROUP})
-        df_memberships[COL_GROUP] = df_memberships[COL_GROUP].str.replace("Bz München", "Bezirk München")   
+        df_memberships[COL_GROUP] = df_memberships[COL_GROUP].str.replace("Bz München", "Bezirk München")
+        df_memberships.columns = df_memberships.columns.str.replace(
+            "Anzahl der Mitglieder nach Altersgruppen.", "Mitglieder Alter."
+        )
 
-    
         # combine the two dataframes to have the membership numbers directly in the location dataframe
         df_combined = pd.merge(df_locations, df_memberships, left_on=COL_GROUP, right_on=COL_GROUP, how="outer")
-        df_combined[COL_GROUP] = df_combined[COL_GROUP].str.replace("LV Bayern", "Landesverband Bayern")   
+        df_combined[COL_GROUP] = df_combined[COL_GROUP].str.replace("LV Bayern", "Landesverband Bayern")
         return True, df_combined
 
     return (prepare_data_nf_membership,)
@@ -151,12 +160,21 @@ def _(prepare_data_nf_membership):
 
 @app.cell
 def _():
-    #df
+    # df
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Karte
+    """)
     return
 
 
 @app.cell
 def _(
+    COL_GROUP,
     df,
     ui_layout_checkbox_cluster,
     ui_layout_color_column,
@@ -167,6 +185,15 @@ def _(
         # https://plotly.com/python/tile-scatter-maps/
         # https://plotly.com/python-api-reference/generated/plotly.express.scatter_map.html
 
+        hover_data = {
+            "lat": False,
+            "lon": False,
+            "Bezirk": True,
+            "Mitglieder.gesamt": True,
+            "Altersdurchschnitt": ":.1f",  # format to one decimal place"
+            "Frauenanteil (Prozent)": ":.1f",  # format to one decimal place"
+        }
+
         # create a scatter map
         fig = px.scatter_map(
             df,
@@ -175,7 +202,11 @@ def _(
             size="Mitglieder.gesamt",  # column which determines the size of the markers
             size_max=ui_layout_marker_size.value,  # maximum mark size (defalt 20)
             color=ui_layout_color_column.value,  # column which determines the color of the markers
-            zoom=6,
+            hover_name=COL_GROUP,  # column to show in the hover tooltip
+            hover_data=hover_data,  # columns to show/hide in the hover tooltip
+            custom_data=[COL_GROUP],  # column to include in the click event data
+            # labels={"Mitglieder.gesamt": "Mitglieder"},  # rename column for legend
+            zoom=6,  # initial zoom level
         )
         # enable clustering of points if they are close together
         if ui_layout_checkbox_cluster.value:
@@ -190,6 +221,7 @@ def _(
 
         return fig
 
+
     _df = get_data_air_traffic()
     fig = plot_map(df)
     return (fig,)
@@ -201,9 +233,65 @@ def _(ui_layout):
     return
 
 
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Diese Karte ist interaktiv. Mit der "Pan" Funktion oben rechts über der Karte, kann diese verschoben werden.
+    Mithilfe von "Box Select" und "Lasso" können bestimmte Ortsgruppen ausgewählt werden, um diese genauer zu untersuchen.
+    """)
+    return
+
+
 @app.cell
-def _(fig):
-    fig
+def _(fig, ui_settings_always_show_mode_bar):
+    _plotly_options = {}
+    if ui_settings_always_show_mode_bar.value:
+        # https://plotly.com/python/configuration-options/
+        _plotly_options["displayModeBar"] = True
+
+    plot = mo.ui.plotly(fig, config=_plotly_options)
+    plot
+    return (plot,)
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Details
+    """)
+    return
+
+
+@app.cell
+def _(plot):
+    plot.value
+    return
+
+
+@app.cell
+def _(COL_GROUP, df, plot):
+    df_details = df
+    # Get user selection if available
+    selection = plot.value
+    if selection is not None and len(selection) > 0:
+        selected_groups = pd.DataFrame(selection)
+        df_details = df_details[df_details["lat"].isin(selected_groups["lat"]) & df_details["lon"].isin(selected_groups["lon"])]  # filter the details dataframe to the selected group
+
+    # remove uninteresting columns
+    _remove_columns = ["lat", "lon", "Postleitzahl", "Stadt", "Strasse", "Koordinaten Kommentar"]
+    df_details = df_details[[col for col in df_details.columns if col not in _remove_columns]]
+
+    # reorder columns for interesting columns first then the rest
+    df_details = df_details[
+        [COL_GROUP, "Bezirk", "Mitglieder.gesamt", "Altersdurchschnitt", "Frauenanteil (Prozent)"]
+        + [
+            col
+            for col in df_details.columns
+            if col not in [COL_GROUP, "Bezirk", "Mitglieder.gesamt", "Altersdurchschnitt", "Frauenanteil (Prozent)"]
+        ]
+    ]
+
+    mo.ui.table(df_details, freeze_columns_left=[COL_GROUP])
     return
 
 
@@ -213,24 +301,25 @@ def _():
     ui_layout_checkbox_cluster = mo.ui.checkbox(label="Gruppieren", value=True)
 
     ui_layout_color_column = mo.ui.dropdown(
-        label="Farbkategorie", 
-        options=
-        {
-            "Einfarbig": None, 
-            "Bezirk": "Bezirk", 
-            "Altersdurchschnitt": "Altersdurchschnitt", 
-            "Frauenanteil": "Frauenanteil (Prozent)"
-        }, 
-        value="Bezirk"
+        label="Farbkategorie",
+        options={
+            "Einfarbig": None,
+            "Bezirk": "Bezirk",
+            "Altersdurchschnitt": "Altersdurchschnitt",
+            "Frauenanteil": "Frauenanteil (Prozent)",
+        },
+        value="Bezirk",
     )
 
     ui_layout_marker_size = mo.ui.slider(start=10, stop=200, step=10, label="Kreisgröße", value=100)
 
-    ui_layout = mo.hstack([
-        ui_layout_checkbox_cluster,
-        ui_layout_color_column,
-        ui_layout_marker_size,
-    ])
+    ui_layout = mo.hstack(
+        [
+            ui_layout_checkbox_cluster,
+            ui_layout_color_column,
+            ui_layout_marker_size,
+        ]
+    )
     return (
         ui_layout,
         ui_layout_checkbox_cluster,
@@ -243,14 +332,22 @@ def _():
 def _():
     # Settings UI elements
     ui_settings_map_height = mo.ui.number(label="Höhe der Karte", value=700, start=300, stop=2000)
+    ui_settings_always_show_mode_bar = mo.ui.checkbox(label="Karten Tool Bar immer anzeigen", value=True)
     ui_settings_show_raw_df = mo.ui.checkbox(label="Rohdaten anzeigen", value=False)
 
-    ui_settings = mo.vstack([
+    ui_settings = mo.vstack(
+        [
+            ui_settings_map_height, 
+            ui_settings_always_show_mode_bar,
+            ui_settings_show_raw_df,
+        ]
+    )
+    return (
+        ui_settings,
+        ui_settings_always_show_mode_bar,
         ui_settings_map_height,
-        ui_settings_show_raw_df
-    ])
-
-    return ui_settings, ui_settings_map_height, ui_settings_show_raw_df
+        ui_settings_show_raw_df,
+    )
 
 
 @app.cell
