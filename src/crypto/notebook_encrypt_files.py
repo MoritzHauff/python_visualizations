cfd9a40493c276b706f9017d8ab62629825a8028
MoritzH <29129441+MoritzHauff@users.noreply.github.com>
2026-06-05 10:41:34 +0200

    ADD example notebook for file encryption/decryption

---
+++ src/crypto/notebook_encrypt_files.py
@@ -0,0 +1,55 @@
+import marimo
+
+__generated_with = "0.21.1"
+app = marimo.App(width="medium")
+
+with app.setup:
+    import json
+    import logging
+    import marimo as mo
+    from crypto.encryption import get_key, write_encrypted, read_encrypted
+
+    #logging.basicConfig(level=logging.DEBUG)
+    #import logging
+    #import sys
+    #
+    #logger = logging.getLogger("myapp")
+    #logger.setLevel(logging.DEBUG)
+    #
+    #handler = logging.StreamHandler(sys.stdout)
+    #handler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+    #
+    ## Avoid duplicate logs if re-running the cell
+    #if not logger.handlers:
+    #    logger.addHandler(handler)
+
+
+@app.cell
+def _():
+    PASSWORD = "STRONG!Password"
+    key = get_key(PASSWORD, "src/crypto/test.salt")
+    key
+    return (key,)
+
+
+@app.cell
+def _(key):
+    example_data = {"example": "This is some example data to encrypt."}
+
+    with open("src/crypto/example_data.json", "w") as f:
+        json.dump(example_data, f)
+
+    write_encrypted("src/crypto/example_data.enc", key, json.dumps(example_data).encode())
+    return
+
+
+@app.cell
+def _(key):
+    _data = read_encrypted("src/crypto/example_data.enc", key)
+    data = json.loads(_data.decode())
+    data
+    return
+
+
+if __name__ == "__main__":
+    app.run()
