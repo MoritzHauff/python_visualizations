209d5e2e0b9c5e7903078fdfb3898e2e4a7bdae1
MoritzH <29129441+MoritzHauff@users.noreply.github.com>
2026-06-05 15:33:14 +0200

    ADD download github raw content

--- src/map_bubble/explore_nf_membership_clusters.py
+++ src/map_bubble/explore_nf_membership_clusters.py
@@ -4,9 +4,14 @@ __generated_with = "0.21.1"
 app = marimo.App(width="medium")
 
 with app.setup:
+    import io
+    import requests
     import marimo as mo
     import pandas as pd
     import plotly.express as px
+    import plotly.graph_objects as go
+
+    from crypto.encryption import get_key, read_encrypted_json
 
 
 @app.cell(hide_code=True)
@@ -18,7 +23,34 @@ def _():
 
 
 @app.function
-def get_data() -> pd.DataFrame:
+def get_data_air_traffic() -> pd.DataFrame:
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv"
+    )
+    return df
+
+
+@app.function
+def download_file(url: str) -> bytes:
+    """Downloads a file from the specified URL and provide it as IO buffer."""
+    query_parameters = {}
+    response = requests.get(url, params=query_parameters)
+    if response.status_code != 200:
+        raise Exception(f"Failed to download file from {url}. Status code: {response.status_code}")
+    return response.content
+
+
+@app.cell
+def _():
+    #file = download_file("https://WRONG_URL")
+    file = download_file("https://raw.githubusercontent.com/MoritzHauff/python_visualizations/refs/heads/main/src/map_bubble/nf_membership/nf_membership_data.enc")
+    file
+
+    return
+
+
+@app.function
+def get_data_nf_membership() -> pd.DataFrame:
     df = pd.read_csv(
         "https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv"
     )
@@ -26,27 +58,33 @@ def get_data() -> pd.DataFrame:
 
 
 @app.cell
-def _():
+def _(get_data):
+    def plot_map(df: pd.DataFrame) -> go.Figure:
+        # https://plotly.com/python/tile-scatter-maps/
+        # https://plotly.com/python-api-reference/generated/plotly.express.scatter_map.html
+    
+        # create a scatter map
+        fig = px.scatter_map(
+            df,
+            lat="lat",  # latitude column
+            lon="long",  # longitude column
+            size="cnt",  # size of the markers based on the 'cnt' column
+            size_max=100,  # maximum mark size (defalt 20)
+            zoom=3,
+        )
+        # enable clustering of points if they are close together
+        fig.update_traces(cluster=dict(enabled=True))
+    
+        # update the map layout (underlying tile provider)
+        # https://plotly.com/python/tile-map-layers/
+        fig.update_layout(map_style="open-street-map")
+
+        return fig
+    
     df = get_data()
-
-    # https://plotly.com/python/tile-scatter-maps/
-    fig = px.scatter_map(
-        df,
-        lat="lat",  # latitude column
-        lon="long",  # longitude column
-        size="cnt",  # size of the markers based on the 'cnt' column
-        size_max=100,  # maximum mark size (defalt 20)
-        zoom=3,
-    )
-    # enable clustering of points if they are close together
-    fig.update_traces(cluster=dict(enabled=True))
-
-    # update the map layout (underlying tile provider)
-    # https://plotly.com/python/tile-map-layers/
-    fig.update_layout(map_style="open-street-map")
-
+    fig = plot_map(df)
     fig.show()
-    return
+    return (df,)
 
 
 if __name__ == "__main__":
