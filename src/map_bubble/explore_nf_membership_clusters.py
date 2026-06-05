277a8bfd82be001803f40c9a65596a562dc90e77
MoritzH <29129441+MoritzHauff@users.noreply.github.com>
2026-06-05 15:01:25 +0200

    ADD example scatter_map

---
+++ src/map_bubble/explore_nf_membership_clusters.py
@@ -0,0 +1,34 @@
+import marimo
+
+__generated_with = "0.21.1"
+app = marimo.App(width="medium")
+
+with app.setup:
+    import marimo as mo
+    import pandas as pd
+    import plotly.express as px
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    # Naturfreunde Mitgliederzahlen nach Ortsgruppe und Alter
+    """)
+    return
+
+
+@app.cell
+def _():
+    # https://plotly.com/python/tile-scatter-maps/
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv"
+    )
+    fig = px.scatter_map(df, lat="lat", lon="long", size="cnt", zoom=3)
+    fig.update_traces(cluster=dict(enabled=True))
+    fig.show()
+    return
+
+
+if __name__ == "__main__":
+    app.run()
