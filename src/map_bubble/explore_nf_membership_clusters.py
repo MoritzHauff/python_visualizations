172b33b8e5e58060a5fe362c2b72d44aa4d5651e
MoritzH <29129441+MoritzHauff@users.noreply.github.com>
2026-06-05 16:21:52 +0200

    FIX import and clean NF membership data

--- src/map_bubble/explore_nf_membership_clusters.py
+++ src/map_bubble/explore_nf_membership_clusters.py
@@ -57,24 +57,17 @@ def download_file(url: str) -> bytes:
     return response.content
 
 
-@app.cell
-def _(ui_text_password):
-    _df = download_data(ui_text_password.value)
-    return
-
-
 @app.function
-def download_data(password) -> pd.DataFrame:
+def download_data(password) -> dict:
+    """Downloads the encrypted data file and the corresponding salt, derives the encryption key using the provided password, and decrypts the data."""
     salt = download_file("https://raw.githubusercontent.com/MoritzHauff/python_visualizations/refs/heads/main/src/map_bubble/nf_membership/nf_membership_data.salt")
-
-    print(f"Accessed salt successfully: {salt} type: {type(salt)}")
+    # print(f"Accessed salt successfully: {salt} type: {type(salt)}")
 
     file = download_file("https://raw.githubusercontent.com/MoritzHauff/python_visualizations/refs/heads/main/src/map_bubble/nf_membership/nf_membership_data.enc")
 
     # Only after both downloads were sucessful, we can proceed to get the key and decrypt the data
     key = get_key(password, salt)
 
-    #file_stream = io.BytesIO(file)
     fernet = Fernet(key)
     try:
         data_bytes = fernet.decrypt(file)
@@ -83,6 +76,60 @@ def download_data(password) -> pd.DataFrame:
     data_json = json.loads(data_bytes.decode())
     
     return data_json
+
+
+@app.cell
+def _():
+    COL_GROUP = "Gruppenbez."
+    return (COL_GROUP,)
+
+
+@app.cell
+def _(COL_GROUP, ui_text_password):
+    def prepare_data() -> tuple[bool, pd.DataFrame]:
+        try:
+            data_dict = download_data(ui_text_password.value)
+            df_locations = pd.DataFrame.from_records(data_dict["group_locations"])
+            df_memberships = pd.DataFrame.from_records(data_dict["group_memberships"])
+        except Exception as e:
+            print(f"**Fehler:** {str(e)}")
+            return False, pd.DataFrame(), True
+
+        # validate data
+        if len(df_locations) == 0 or len(df_memberships) == 0:
+            print("**Fehler:** Die heruntergeladenen Daten sind leer.")
+            return False, pd.DataFrame()
+
+        if len(df_locations) != len(df_memberships):
+            print("**Warnung:** Die Anzahl der Ortsgruppen in den beiden Datensätzen stimmt nicht überein. Es könnte sein, dass einige Gruppen in einem Datensatz fehlen.")
+        
+        # clean data
+        df_locations = df_locations.rename(columns={"Unnamed: 6": "Koordinaten Kommentar"})
+        df_locations = df_locations.rename(columns={"Gruppenbezeichnung": COL_GROUP})
+        #df_locations[COL_GROUP] = df_locations[COL_GROUP].str.replace("Bezirk München", "Bz München")   
+
+        df_memberships = df_memberships.rename(columns={"Gruppenbezeichnung": COL_GROUP})
+        df_memberships[COL_GROUP] = df_memberships[COL_GROUP].str.replace("Bz München", "Bezirk München")   
+
+    
+        # combine the two dataframes to have the membership numbers directly in the location dataframe
+        df_combined = pd.merge(df_locations, df_memberships, left_on=COL_GROUP, right_on=COL_GROUP, how="outer")
+        df_combined[COL_GROUP] = df_combined[COL_GROUP].str.replace("LV Bayern", "Landesverband Bayern")   
+        return True, df_combined
+
+    return (prepare_data,)
+
+
+@app.cell
+def _(prepare_data):
+    data_success, df = prepare_data()
+    return (df,)
+
+
+@app.cell
+def _(df):
+    mo.ui.dataframe(df)
+    return
 
 
 @app.function
@@ -94,7 +141,7 @@ def get_data_nf_membership() -> pd.DataFrame:
 
 
 @app.cell
-def _(get_data):
+def _():
     def plot_map(df: pd.DataFrame) -> go.Figure:
         # https://plotly.com/python/tile-scatter-maps/
         # https://plotly.com/python-api-reference/generated/plotly.express.scatter_map.html
@@ -117,8 +164,8 @@ def _(get_data):
 
         return fig
 
-    df = get_data()
-    fig = plot_map(df)
+    _df = get_data_air_traffic()
+    fig = plot_map(_df)
     fig.show()
     return
 
